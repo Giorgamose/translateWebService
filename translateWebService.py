d6f223a0704b99ae77492b9e24add4e12c0215c2
George Majewski <36228883+Giorgamose@users.noreply.github.com>
2023-03-07 20:13:01 +0400

    Create translateWebService.py

---
+++ translateWebService.py
@@ -0,0 +1,25 @@
+import requests
+import html
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+url = 'https://translation.googleapis.com/language/translate/v2'
+
+@app.route('/translate', methods=['POST'])
+def translate_text():
+    input_text = request.json.get('text', '')
+    if not input_text:
+        return jsonify({'error': 'No input text provided.'})
+    params = {
+        'key': 'AIzaSyBri2ufYJVFh5r4EBfxkpTC3FNyaujZgEo',
+        'target': 'en',
+        'source': 'ka',
+        'q': input_text
+    }
+    response = requests.post(url, params=params).json()
+    translated_text = response['data']['translations'][0]['translatedText']
+    updated_text = html.unescape(translated_text)
+    return jsonify({'result': updated_text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
