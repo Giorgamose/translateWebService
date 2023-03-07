eebc8adce95b3480d5fea7c518c1a8e2e820dbbc
Giorgi Maevski <gmaevski@tbcbank.com.ge>
2023-03-07 17:40:05 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+def print_hi():
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    arg1 = sys.argv["Mama I love you"]
+    python translateWebService.py 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
