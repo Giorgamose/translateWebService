c3770cc77191cf4fcb95868f82d08a3841491431
George Majewski <36228883+Giorgamose@users.noreply.github.com>
2023-03-09 11:08:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ url = 'https://translation.googleapis.com/language/translate/v2'
 @app.route('/translate', methods=['POST'])
 def translate_text():
     input_text = request.json.get('text', '')
+    target = request.json.get('target', '')
+    source = request.json.get('source', '')
     if not input_text:
         return jsonify({'error': 'No input text provided.'})
     params = {
-        'key': 'GoogleAppKey',
-        'target': 'en',
-        'source': 'ka',
+        'key': 'AIzaSyBri2ufYJVFh5r4EBfxkpTC3FNyaujZgEo',
+        'target': target ,
+        'source': source ,
         'q': input_text
     }
     response = requests.post(url, params=params).json()
